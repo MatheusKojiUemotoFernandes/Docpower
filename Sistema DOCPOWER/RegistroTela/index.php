98ddc6854b436b4f521c8b628da784b5e217d2d1
Miguel B. P <96029590+miguel-b-p@users.noreply.github.com>
2024-05-09 01:09:25 +0000

    Update and rename index.html to index.php

---
+++ Sistema DOCPOWER/RegistroTela/index.php
@@ -0,0 +1,140 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="./style.css">
+    
+    <title>Tela Login</title>
+</head>
+
+<body>
+    <div class="container">
+        <section class="header">
+            <?php
+                session_start();
+                if(isset($_SESSION['sucesso_cadastro'])) {
+                    echo '<h2>'.$_SESSION['sucesso_cadastro'].'</h2>';
+                    unset($_SESSION['sucesso_cadastro']);
+                } elseif (isset($_SESSION['erro_cadastro'])){
+                    echo '<h2>'.$_SESSION['erro_cadastro'].'</h2>';
+                    unset($_SESSION['erro_cadastro']);
+                } else {
+                    echo '<h2>Cadastrar:</h2>';
+                }
+                ?>
+        </section>
+        <form action="registro.php" method="POST" class="form" id="form">
+            <div class="form-content">
+                <label for="docu"></label>
+                <input
+                    name="docu"
+                    type="text"
+                    id="docu"
+                    placeholder="CNPJ/CPF:"
+                />
+                <?php
+                if(isset($_SESSION['documento'])) {
+                    echo '<a id="error">'.$_SESSION['documento'].'</a>';
+                    unset($_SESSION['documento']);
+                } else {
+                    echo '<a>Aqui vai a mensagem de erro</a>';
+                }
+
+                ?>
+            </div>
+            <div class="form-content">
+                <label for="nome"></label>
+                <input
+                    name="nome"
+                    type="text"
+                    id="nome"
+                    placeholder="Nome completo:"
+                />
+                <?php
+                if(isset($_SESSION['nome'])) {
+                    echo '<a id="error">'.$_SESSION['nome'].'</a>';
+                    unset($_SESSION['nome']);
+                } else {
+                    echo '<a>Aqui vai a mensagem de erro</a>';
+                }
+                ?>
+            </div>
+
+            <div class="form-content">
+                <label for="tele"></label>
+                <input
+                    name="tele"
+                    type="tel"
+                    id="tele"
+                    placeholder="Telefone:"
+                />
+                <?php
+                if(isset($_SESSION['tele'])) {
+                    echo '<a id="error">'.$_SESSION['tele'].'</a>';
+                    unset($_SESSION['tele']);
+                } else {
+                    echo '<a>Aqui vai a mensagem de erro</a>';
+                }
+                ?>
+            </div>
+
+            <div class="form-content">
+                <label for="email"></label>
+                <input
+                    name="email"
+                    type="email"
+                    id="email"
+                    placeholder="E-mail:"
+                />
+                <?php
+                if(isset($_SESSION['email'])) {
+                    echo '<a id="error">'.$_SESSION['email'].'</a>';
+                    unset($_SESSION['email']);
+                } else {
+                    echo '<a>Aqui vai a mensagem de erro</a>';
+                }
+                ?>
+            </div>
+
+            <div class="form-content">
+                <label for="password"></label>
+                <input
+                    name="senha"
+                    type="password"
+                    id="password"
+                    placeholder="Senha:"
+                />
+                <?php
+                if(isset($_SESSION['senha'])) {
+                    echo '<a id="error">'.$_SESSION['senha'].'</a>';
+                    unset($_SESSION['senha']);
+                } else {
+                    echo '<a>Aqui vai a mensagem de erro</a>';
+                }
+                ?>
+            </div>
+
+            <div class="form-content">
+                <label for="password-confirmation"></label>
+                <input
+                    type="password"
+                    id="password-confirmation"
+                    placeholder="Confirme sua senha:"
+                />
+                <a>Aqui vai a mensagem de erro</a>
+            </div>
+            <button type="submit"><span>Criar conta</span></button>
+        </form>
+        <div class="register-link">
+            <p>Já cadastrado? <a href="file:///C:/Users/japis/OneDrive/%C3%81rea%20de%20Trabalho/Sistema%20DOCPOWER/Telalogin/index.html">Acessar conta</a></p>
+        </div>
+    </div>
+</body>
+</html>
