fcb733796d725e3786e806996fd5d462ca5fcdb8
Miguel B. P <96029590+miguel-b-p@users.noreply.github.com>
2024-05-10 17:27:07 +0000

    Banco De Dados

--- DOCPOWER/docpower.sql
+++ DOCPOWER/docpower.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2024 at 07:24 PM
+-- Generation Time: May 10, 2024 at 07:26 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -36,13 +36,6 @@ CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `data_cadastro` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `usuario`
---
-
-INSERT INTO `usuario` (`docu`, `nome`, `tele`, `email`, `senha`, `id`, `data_cadastro`) VALUES
-('11111111111', 'Miguel', '1234567891011', '123@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$TWV5TUV4WnFHWi92MlcwZg$X2qlUIX1zxqv8gLF+xJVK3/lNvQIDvWRuM+0BoidQwI', 35, '2024-05-10 12:27:07');
 
 --
 -- Indexes for dumped tables
